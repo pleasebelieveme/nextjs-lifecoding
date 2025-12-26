8d1d207a9d30126eb1a92cfadbb28f85e6e1e3b5
believeme <cain1227@naver.com>
2025-12-26 13:40:47 +0900

    lesson: 12강 글생성 및 디비 저장 확인

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -1,5 +1,38 @@
+"use client"
+
+import { useRouter } from "next/navigation";
+
 export default function Create() {
+    const router = useRouter();
     return (
-        <>Create</>
+        <form onSubmit={(event)=>{
+            event.preventDefault();
+            const title = event.target.title.value;
+            const body = event.target.body.value;
+            const options = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({title, body})
+            } 
+            fetch(`http://localhost:9999/topics`, options)
+                .then(response => response.json())
+                .then(result => {
+                    console.log(result)
+                    const lastid = result.id;
+                    router.push(`/read/${lastid}`);
+                });
+        }}>
+            <p>
+                <input type="text" name="title" placeholder="title" />
+            </p>
+            <p>
+                <textarea name="body" placeholder="body" />
+            </p>
+            <p>
+                <input type="submit" value="create" />
+            </p>
+        </form>
     )
 }
